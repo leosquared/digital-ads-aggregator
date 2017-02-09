1954b5357ecc00c7760e618375c831bbde810873
leosquared <leo14358@gmail.com>
2017-02-09 13:31:08 -0500

    hello world

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-
-from os import environ
 from flask import Flask
-
 app = Flask(__name__)
-app.run(environ.get('PORT'))
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
