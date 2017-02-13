f75df273429b7101d5d8428fec4b57e1b3a33291
leosquared <leo14358@gmail.com>
2017-02-13 12:12:29 -0500

    html welcome text change

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hello World!"
+	return "This app is working!"
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
